40206e9ce5c5fbeb471c9963e7e3326930040715
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-03 19:57:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,4 +24,16 @@ def actor
     }
   end 
   
+  def monopoly_with_third_tier
+    monopoly = {
+    railroads: {
+     pieces: 4,
+     names: {},
+     rent_in_dollars: {
+       one_piece_owned: 25
+     }
+      }
+    }
+  end 
+  
   
